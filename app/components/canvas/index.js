ed4f3d855ea8c8d9b77a09ca7b05e28f6ff974ee
Emmanuel Makau <emmanuelmakau90@gmail.com>
2024-12-22 12:13:58 +0300

    � Converted canvas export file to Next.js format �

--- app/components/canvas/index.js
+++ app/components/canvas/index.js
@@ -1,6 +1,9 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
+// app/components/index.js
+'use client';
+
+import EarthCanvas from "./EarthCanvas";
+import BallCanvas from "./BallCanvas";
+import ComputersCanvas from "./ComputersCanvas";
+import StarsCanvas from "./StarsCanvas";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
